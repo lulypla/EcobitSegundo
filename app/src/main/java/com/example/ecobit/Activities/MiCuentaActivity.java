481d8bff8040308f3e3a83c30d405639ee12ab3d
javittop <jpereyracreus@gmail.com>
2020-03-16 11:30:26 -0300

    Pantalla de Mi Perfil y Mi Cuenta.
    Falta conectar con demás pantallas

---
+++ app/src/main/java/com/example/ecobit/Activities/MiCuentaActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecobit.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecobit.R;
+
+public class MiCuentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_cuenta);
+    }
+}

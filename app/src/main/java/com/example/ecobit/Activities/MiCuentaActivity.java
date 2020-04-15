e4b5b4a4b29f5b1969cf4cebc2a252f76c1409d0
Debray Peralta <debray.peralta@gmail.com>
2020-04-15 19:47:15 -0300

    Baja de usuario

--- app/src/main/java/com/example/ecobit/Activities/MiCuentaActivity.java
+++ app/src/main/java/com/example/ecobit/Activities/MiCuentaActivity.java
@@ -1,13 +1,24 @@
 package com.example.ecobit.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.ecobit.Model.User;
 import com.example.ecobit.R;
+import com.example.ecobit.Services.APIService;
+import com.example.ecobit.Services.UserService;
 import com.example.ecobit.utils.Sesion;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MiCuentaActivity extends AppCompatActivity {
 
@@ -30,6 +41,68 @@ public class MiCuentaActivity extends AppCompatActivity {
         Sesion.borrar(this);
         startActivity(logout);
         overridePendingTransition(R.anim.zoom_back_in, R.anim.zoom_back_out);
+    }
+
+    public void darseDeBaja(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Confirmar baja");
+        builder.setMessage("Esta seguro que quiere darse de baja?");
+
+        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                requestBaja();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void requestBaja() {
+        final ProgressDialog spinner = ProgressDialog.show(MiCuentaActivity.this, "Procesando baja",
+                "Enviando datos, por favor espere...", true);
+        User user = Sesion.getUser(MiCuentaActivity.this);
+        UserService userService = APIService.getApi().create(UserService.class);
+        Call<User> userBaja = userService.bajaUser(user);
+        final Intent intentEmpecemos = new Intent(this, EmpecemosActivity.class);
+        userBaja.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                if (user != null && user.getEmail() != null) {
+                    intentEmpecemos.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intentEmpecemos);
+                    overridePendingTransition(R.anim.zoom_back_in, R.anim.zoom_back_out);
+                    spinner.cancel();
+                    Toast.makeText(MiCuentaActivity.this, "Usuario dado de baja con exito", Toast.LENGTH_LONG).show();
+
+                    finish();
+
+                } else {
+                    Toast.makeText(MiCuentaActivity.this, "Error en la baja", Toast.LENGTH_LONG).show();
+                    spinner.cancel();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                spinner.cancel();
+                Toast.makeText(MiCuentaActivity.this, "Error en Pedido, Compruebe su conexion a internet", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/ecobit/Services/UserService.java
+++ app/src/main/java/com/example/ecobit/Services/UserService.java
@@ -1,30 +1,20 @@
 package com.example.ecobit.Services;
 
-import android.widget.EditText;
-
 import com.example.ecobit.Model.User;
 
 import java.util.List;
-import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FieldMap;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.Headers;
+import retrofit2.http.HTTP;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
 import retrofit2.http.Query;
-import retrofit2.http.QueryMap;
 
 public interface UserService {
     @GET("login")
     Call<User> user(@Query("userId") String userId);
 
-    @GET("api/ecobit/user")
-    Call<List<User>> getUsers();
 
     @POST("login")
     Call<User> login(@Body User user);
@@ -35,20 +25,8 @@ public interface UserService {
     @POST("user/updatePhoto")
     Call<User> postPhoto(@Body User user);
 
-    @POST("user/updatePhoto")
-    Call<User> post000Photo(@Query("email") String email,
-                            @Query("foto") String foto);
+    @HTTP(method = "DELETE", path = "/api/v1/user", hasBody = true)
+    Call<User> bajaUser(@Body User user);
 
-
-    @POST("registro")
-    Call<User> signI2(
-            @Field("email") EditText email,
-            @Field("password") EditText pass,
-            @Field("nombre") EditText nombre,
-            @Field("apellido") EditText apellido,
-            @Field("tel") EditText tel,
-            @Field("nro_doc") String nro_documento,
-            @Field("tipo_doc") String tipo_doc,
-            @Field("fecha_nac") String fecha_nac);
 
 }
